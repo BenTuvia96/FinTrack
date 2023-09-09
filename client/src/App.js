507ce9ac7be8d18f5cb35c7efdc802704e04c3d6
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-09-09 19:54:57 +0300

    basic front page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const app_description = (
+    <div>
+      Welcome to Fin Track – your personal gateway to financial clarity.
+      <br />
+      Let's simplify your money journey together.
+      <br />
+      Dive in and take control of your finances today!
+    </div>
+  );
   return (
     <div className="App">
-      <h1>React App</h1>
+      <img src="/White_Logo.png" alt="logo" />
+      <p className="app_description">{app_description}</p>
+      <div className="button-container">
+        <button className="sign_button">Sign In</button>
+        <button className="sign_button">Sign Out</button>
+      </div>
     </div>
   );
 }
