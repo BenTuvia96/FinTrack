54fa02acd34628521ae1ebead5d952ce65affd62
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-09-10 14:36:13 +0300

    added sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
+import SignInForm from "./sign_in_form";
 
-function App() {
-  const app_description = (
+
+
+function Description() {
+  return (
     <div>
-      Welcome to Fin Track – your personal gateway to financial clarity.
-      <br />
-      Let's simplify your money journey together.
-      <br />
-      Dive in and take control of your finances today!
+    Welcome to Fin Track – your personal gateway to financial clarity.
+    <br />
+    Let's simplify your money journey together.
+    <br />
+    Dive in and take control of your finances today!
+  </div>
+  );
+}
+
+function SignInButtons() {
+  return (
+    <div className="button-container">
+      <Link to="/sign_in_form" className="sign_button">Sign In</Link>
+      <Link to="/signup" className="sign_button">Sign Up</Link>
     </div>
   );
+}
+
+function HomePage() {
   return (
     <div className="App">
-    <img src="/White_Logo.svg" alt="logo" className="App-logo" />
-      <p className="app_description">{app_description}</p>
-      <div className="button-container">
-        <button className="sign_button">Sign In</button>
-        <button className="sign_button">Sign Out</button>
-      </div>
+      <img src="/White_Logo.svg" alt="logo" className="App-logo" />
+      <p className="app_description"><Description /></p>
+      <SignInButtons />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign_in_form" element={<SignInForm />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/sign_in_form.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function SignInForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle sign-in logic here (e.g., call an API)
+  };
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
