b5ddfb80653764e55d362dc12e9e78de73457f4d
BenTuvia96 <bentu@post.bgu.ac.il>
2023-09-10 18:16:26 +0300

    added sign in form with styling

--- client/src/sign_in_form.js
+++ client/src/sign_in_form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "./sign_in_form.css"
 
 function SignInForm() {
@@ -11,27 +12,31 @@ function SignInForm() {
   };
 
   return (
-    <div>
+    <div className='form_container'>
+      <img src="/white_logo.svg" alt="App Logo" className="app_logo" />
       <h2>Sign In</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
+        <div className="form_group">
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="form_group">
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <button type="submit">Sign In</button>
       </form>
+      <p>New to Fin Track? <Link to="/signup">Sign up now</Link></p>
     </div>
   );
 }
