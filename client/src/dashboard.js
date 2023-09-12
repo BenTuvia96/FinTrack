027e46155b57a33b55c7df8d3d43366d5c427939
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-09-12 17:21:23 +0300

    changes graph library for dashboard and created dashboard_componentsfolder

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -1,53 +1,25 @@
 import React, { Component } from "react";
-import CanvasJSReact from "@canvasjs/react-charts";
 import "./dashboard.css";
-import TopBar from './top_bar';
-
-
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import TopBar from "./top_bar";
+import { DoughnutChart } from "./dashboard_components/doughnut_chart.jsx";
+import { BarChart } from "./dashboard_components/bar_chart";
+import { AreaChart } from "./dashboard_components/area_chart";
 
 class Dashboard extends Component {
   render() {
-    const data = [
-      { name: "food", y: 40 },
-      { name: "rent", y: 20 },
-      { name: "bills", y: 10 },
-      { name: "entertainment", y: 10 },
-      { name: "other", y: 20 }
-    ];
-
-    const options = {
-      animationEnabled: false,
-      title: {
-        text: "Expenses",
-        fontFamily: "tahoma",
-      },
-      subtitles: [
-        {
-          text: "*balance*",
-          verticalAlign: "center",
-          fontSize: 24,
-          dockInsidePlotArea: true,
-          fontFamily: "tahoma",
-        },
-      ],
-      data: [
-        {
-          type: "doughnut",
-          showInLegend: false,
-          indexLabel: "{name}: {y}",
-          yValueFormatString: "#,###'%'",
-          dataPoints: data,
-        },
-      ],
-      backgroundColor: "transparent",
-    };
     return (
       <div>
         <TopBar header={"Dashboard"}/>
-        <div className="dashboard_container">
-          <div className="chart_container">
-            <CanvasJSChart className="chart" options={options} />
+
+        <div className="dashboard-container">
+          <div className="chart-container">
+            <DoughnutChart />
+          </div>
+          <div className="chart-container">
+            <BarChart />
+          </div>
+          <div className="chart-container">
+            <AreaChart />
           </div>
         </div>
       </div>
