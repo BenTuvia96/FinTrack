665d49469dfc6c19134cf3ce8e1c4b44fac0ff40
BenTuvia96 <bentu@post.bgu.ac.il>
2023-09-14 17:02:37 +0300

    add expanse form

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -3,9 +3,30 @@ import TopBar from "./top_bar";
 import { DoughnutChart } from "./dashboard_components/doughnut_chart.jsx";
 import { BarChart } from "./dashboard_components/bar_chart";
 import { AreaChart } from "./dashboard_components/area_chart";
+import OutcomeInput from "./dashboard_components/add_expanse";
+import IncomeInput from "./dashboard_components/add_income"; // Create a component for income input
 import "./dashboard.css";
 
 class Dashboard extends Component {
+  state = {
+    showOutcomeInput: false,
+    showIncomeInput: false,
+  };
+
+  toggleOutcomeInput = () => {
+    this.setState((prevState) => ({
+      showOutcomeInput: !prevState.showOutcomeInput,
+      showIncomeInput: false, // Close income input when outcome input is opened
+    }));
+  };
+
+  toggleIncomeInput = () => {
+    this.setState((prevState) => ({
+      showIncomeInput: !prevState.showIncomeInput,
+      showOutcomeInput: false, // Close outcome input when income input is opened
+    }));
+  };
+
   render() {
     return (
       <div className="dashboard_page_container">
@@ -20,19 +41,35 @@ class Dashboard extends Component {
           <div className="area-container">
             <AreaChart />
           </div>
-
+          <div className="input-form-container">
+            {this.state.showOutcomeInput && <OutcomeInput />}
+            {this.state.showIncomeInput && <IncomeInput />}
+          </div>
           <div className="income-and-outcome-buttons-container">
             <div className="income-button-container">
-              <button className="material-icons">add</button>
-              <h2 className="add-income-text">
+              <button
+                className="material-icons"
+                onClick={this.toggleIncomeInput}
+              >
+                add
+              </button>
+              <h2 className="add-income-text" onClick={this.toggleIncomeInput}>
                 Add
                 <br />
                 Income
               </h2>
             </div>
             <div className="outcome-button-container">
-              <button className="material-icons">remove</button>
-              <h2 className="add-outcome-text">
+              <button
+                className="material-icons"
+                onClick={this.toggleOutcomeInput}
+              >
+                remove
+              </button>
+              <h2
+                className="add-outcome-text"
+                onClick={this.toggleOutcomeInput}
+              >
                 Add
                 <br />
                 Outcome
