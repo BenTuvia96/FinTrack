7c9cf6fb800804ab066337a40f941337d448407a
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-09-14 11:38:48 +0300

    added animation to dashboard

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import "./dashboard.css";
 import TopBar from "./top_bar";
 import { DoughnutChart } from "./dashboard_components/doughnut_chart.jsx";
 import { BarChart } from "./dashboard_components/bar_chart";
 import { AreaChart } from "./dashboard_components/area_chart";
+import "./dashboard.css";
 
 class Dashboard extends Component {
   render() {
@@ -19,6 +19,25 @@ class Dashboard extends Component {
           </div>
           <div className="area-container">
             <AreaChart />
+          </div>
+
+          <div className="income-and-outcome-buttons-container">
+            <div className="income-button-container">
+              <button className="material-icons">add</button>
+              <h2 className="add-income-text">
+                Add
+                <br />
+                Income
+              </h2>
+            </div>
+            <div className="outcome-button-container">
+              <button className="material-icons">remove</button>
+              <h2 className="add-outcome-text">
+                Add
+                <br />
+                Outcome
+              </h2>
+            </div>
           </div>
         </div>
       </div>
