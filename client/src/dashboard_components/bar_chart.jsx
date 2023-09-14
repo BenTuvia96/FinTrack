9309720ba18427ec48feda1a353732b24bf6ee40
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-09-14 17:02:33 +0300

    dashboard charts dark mode

--- client/src/dashboard_components/bar_chart.jsx
+++ client/src/dashboard_components/bar_chart.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../ThemeContext";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -20,37 +21,77 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Income & Outcome by Month",
-    },
-  },
-};
-
 const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "income",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "outcome",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
+const light_theme_colors = {
+  income: "rgba(255, 99, 132, 0.5)",
+  outcome: "rgba(53, 162, 235, 0.5)",
+};
+
+const dark_theme_colors = {
+  income: "rgba(255, 99, 132, 0.9)",
+  outcome: "rgba(53, 162, 235, 0.9)",
 };
 
 export function BarChart() {
+  const { theme } = useContext(ThemeContext);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "income",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor:
+          theme === "light"
+            ? light_theme_colors.income
+            : dark_theme_colors.income,
+      },
+      {
+        label: "outcome",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor:
+          theme === "light"
+            ? light_theme_colors.outcome
+            : dark_theme_colors.outcome,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          color: theme === "dark" ? "white" : "black",
+        },
+      },
+      title: {
+        display: true,
+        text: "Income & Outcome by Month",
+        color: theme === "dark" ? "white" : "black",
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: theme === "dark" ? "white" : "black",
+        },
+        grid: {
+          color: theme === "dark" ? "rgba(255,255,255,0.1)" : "rgba(0,0,0,0.1)",
+        },
+      },
+      y: {
+        ticks: {
+          color: theme === "dark" ? "white" : "black",
+        },
+        grid: {
+          color: theme === "dark" ? "rgba(255,255,255,0.1)" : "rgba(0,0,0,0.1)",
+        },
+      },
+    },
+  };
+
   return <Bar options={options} data={data} />;
 }
--- client/src/dashboard_components/doughnut_chart.jsx
+++ client/src/dashboard_components/doughnut_chart.jsx
@@ -1,49 +1,73 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../ThemeContext";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "bottom",
-    },
-    title: {
-      display: true,
-      text: "Expenses by Category",
-    },
-  },
-};
+const light_theme_colors = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(153, 102, 255, 0.2)",
+  "rgba(255, 159, 64, 0.2)",
+];
 
-export const data = {
-  labels: ["food", "rent", "bills", "entertainment", "other", "savings"],
-  datasets: [
-    {
-      label: "amount spent in $",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+const dark_theme_colors = [
+  "rgba(255, 99, 132, 0.75)",
+  "rgba(54, 162, 235, 0.75)",
+  "rgba(255, 206, 86, 0.75)",
+  "rgba(75, 192, 192, 0.75)",
+  "rgba(153, 102, 255, 0.75)",
+  "rgba(255, 159, 64, 0.75)",
+];
 
 export function DoughnutChart() {
+  const { theme } = useContext(ThemeContext);
+
+  const data = {
+    labels: ["food", "rent", "bills", "entertainment", "other", "savings"],
+    datasets: [
+      {
+        label: "amount spent in $",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor:
+          theme === "light" ? light_theme_colors : dark_theme_colors,
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          color: theme === "dark" ? "white" : "black",
+        },
+      },
+      title: {
+        display: true,
+        text: "Expenses by Category",
+        color: theme === "dark" ? "white" : "black",
+      },
+      tooltip: {
+        titleFontColor: theme === "dark" ? "white" : "black",
+        bodyFontColor: theme === "dark" ? "white" : "black",
+        footerFontColor: theme === "dark" ? "white" : "black",
+      },
+    },
+  };
+
   return <Doughnut options={options} data={data} />;
 }
