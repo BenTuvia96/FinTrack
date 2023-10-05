661ea280cd0101b4d5f4339e07790496979267dc
Shaked Haisraeli <shaked.haisraeli@mail.huji.ac.il>
2023-10-06 01:17:24 +0300

    fixing menu styling bug & adding markup on menu for current page to improve UX

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import "./Menu.css";
 
 function Menu({ theme, toggleTheme }) {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isActive = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
 
   const signOut = () => {
     // Remove token from local storage
     localStorage.removeItem("token");
-
-    // Optionally, clear any user-specific state data here
 
     navigate("/");
   };
@@ -21,16 +23,16 @@ function Menu({ theme, toggleTheme }) {
       </label>
       <nav>
         <ul>
-          <li>
+          <li className={isActive("/profile")}>
             <i className="material-icons">person</i>Profile
           </li>
           <Link to="/dashboard">
-            <li>
+            <li className={isActive("/dashboard")}>
               <i className="material-icons">wallet</i>Dashboard
             </li>
           </Link>
           <Link to="/transactions">
-            <li>
+            <li className={isActive("/transactions")}>
               <i className="material-icons">paid</i>Transactions{" "}
             </li>
           </Link>
